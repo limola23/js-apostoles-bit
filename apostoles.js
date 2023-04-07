d95aa523a3b2deefea7eae6a1e6ec5447b86a905
limola23 <molanolili23@gmail.com>
2023-04-06 19:26:47 -0500

    corrigiendo posiciónes en el array

--- apostoles.js
+++ apostoles.js
@@ -2,4 +2,4 @@ let num = prompt("Ingrese número");
 
 let apostoles = ["Pedro", "Santiago el anciano", "Juan", "Andrés", "Bartolomé", "Santiago el menor", "Judas Iscariote", "Judas Tadeo", "Mateo", "Felipe", "Simón", "Tomás"];
 
-alert ("El apostol 4 es: " + (apostoles[num]));
+alert ("El apostol"+ " " + num + " " + "es: " + (apostoles[num-1]));
