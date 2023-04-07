51fc16af56080f9707a2a4018e27b41707b460f7
limola23 <molanolili23@gmail.com>
2023-04-06 19:19:49 -0500

    entrega 05/04

---
+++ apostoles.js
@@ -0,0 +1,5 @@
+let num = prompt("Ingrese número");
+
+let apostoles = ["Pedro", "Santiago el anciano", "Juan", "Andrés", "Bartolomé", "Santiago el menor", "Judas Iscariote", "Judas Tadeo", "Mateo", "Felipe", "Simón", "Tomás"];
+
+alert ("El apostol 4 es: " + (apostoles[num]));
